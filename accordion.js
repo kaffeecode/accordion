7731bc05bd1251c545c2de84fd3259f1b3ece05b
2pix <topix@mail.de>
2016-03-10 08:51:41 +0100

    initial commit

---
+++ accordion.js
@@ -0,0 +1,31 @@
+(function initialize($) {
+
+  $.fn.kcAccordion = function kcAccordion(
+    { closeOther } = {}) {
+    const that = this;
+
+    $(that).children('.kc-head').on('click', function headClicked() {
+      const next = $(this).next('.kc-content');
+      const expanded = next.hasClass('kc-expanded');
+
+      if(closeOther) {
+        $(that).children('.kc-content')
+          .removeClass('kc-expanded')
+          .css('max-height', '');
+      }
+
+      if(!expanded) {
+        next
+          .addClass('kc-expanded')
+          .css('max-height', `${next[0].scrollHeight}px`);
+      }
+      else if(!closeOther) {
+        next
+          .removeClass('kc-expanded')
+          .css('max-height', '');
+      }
+    });
+  };
+})(jQuery);
+
+export {};
